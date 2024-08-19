4d5e0aaad109600e7f82d3bc79c5dc65b1eb332a
root <2659289064@qq.com>
2024-08-20 01:56:15 +0800

    完成鼠标消息处理

--- RemoteCtrl/RemoteCtrl/ServerSocket.h
+++ RemoteCtrl/RemoteCtrl/ServerSocket.h
@@ -125,6 +125,19 @@ public:
 };
 #pragma pack(pop)
 
+//鼠标操作结构体
+typedef struct MouseEvent {
+	MouseEvent() {
+		nAction = 0;
+		nButton = -1;
+		ptXY.x = 0;
+		ptXY.y = 0;
+	}
+	WORD nAction;	//点击，移动，双击
+	WORD nButton;	//左键，右键，中键
+	POINT ptXY;	//坐标
+}MOUSEEV, *PMOUSEEV;
+
 //构造单例模式
 class CServerSocket
 {
@@ -217,6 +230,12 @@ public:
 			return TRUE;
 		}
 		return FALSE;
+	}
+	BOOL GetMouseEvent(MOUSEEV& mouse) {
+		if (m_packet.sCmd == 5) {
+			memcpy(&mouse, m_packet.strData.c_str(), sizeof(MOUSEEV));
+			return TRUE;
+		}
 	}
 private:
 	SOCKET m_sock, m_client;
